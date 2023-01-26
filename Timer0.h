561795c85aa964b4af56e4479677c842d66cffa7
Fernandot989 <110882090+Fernandot989@users.noreply.github.com>
2023-01-25 20:46:13 -0600

    Create Timer0.h

---
+++ Timer0.h
@@ -0,0 +1,16 @@
+/* 
+ * File: Lab1
+ * Author: Oscar Fernando Donis Martínez
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef Timer0_H
+#define	Timer0_H
+
+  
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+

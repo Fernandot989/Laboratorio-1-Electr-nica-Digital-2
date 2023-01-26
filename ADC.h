34ad8d0dbd1b72c51eaae0bf183f35e9d8699118
Fernandot989 <110882090+Fernandot989@users.noreply.github.com>
2023-01-25 20:47:20 -0600

    Create ADC.h

---
+++ ADC.h
@@ -0,0 +1,16 @@
+/* 
+ * File: Lab1
+ * Author: Oscar Fernando Donis Martínez
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef ADC_H
+#define	ADC_H
+
+  
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
